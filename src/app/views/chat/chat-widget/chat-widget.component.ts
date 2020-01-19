f18fafa704ff3d6a037e5a0de4d3114b586f968b
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2020-01-19 17:29:30 +0100

    Fix gmart-chat-ws uri

--- src/app/views/chat/chat-widget/chat-widget.component.ts
+++ src/app/views/chat/chat-widget/chat-widget.component.ts
@@ -15,7 +15,7 @@ const rand = max => Math.floor(Math.random() * max)
 })
 export class ChatWidgetComponent implements OnInit {
   private stompClient;
-  private serverUrl = 'http://localhost:9081/kafkka/api'
+  private serverUrl = 'http://gmart-ws-chat.herokuapp.com/kafkka/api'
   private receivedMessage: string = '';
   @ViewChild('bottom', null) bottom: ElementRef
   @Input() public theme: 'blue' | 'grey' | 'red' = 'blue'
