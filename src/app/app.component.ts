1d318adb7197ed3705b0a5e4e40911ad401e9394
Youssef TOUNOUSSI <youssef.tounoussi@gmail.com>
2020-11-16 17:20:06 +0100

    update AppCompoent URLs for Heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  // static API_URL = 'https://gmart-communicator-ws.herokuapp.com/communicatorws';
-  static API_URL = "http://localhost:8088/communicatorws";
+   static API_URL = 'https://gmart-communicator-ws.herokuapp.com/communicatorws';
+  //static API_URL = "http://localhost:8088/communicatorws";
   // communicatorws/api/gmartws/api/authenticate
  // static API_URL = 'https://gmart-communicator-ws.herokuapp.com/communicatorws';
  static CHAT_API_WEBSOCKETS_URL = 'https://gmart-ws-chat-websockets.herokuapp.com/gmart-chat-ws/gmart-chat-ws'
+ //static CHAT_API_WEBSOCKETS_URL = 'http://localhost:9091/gmart-chat-ws/gmart-chat-ws'
 
   title = "DMART";
   // To close the electron app
